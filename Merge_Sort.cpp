473d9528b2d499811a292f7cd82338c45a0fafee
Mehir_007 <mehirrouth20000@gmail.com>
2023-04-26 23:21:39 +0530

    Recursion

--- Merge_Sort.cpp
+++ Merge_Sort.cpp
@@ -1,6 +1,8 @@
 /*
 Merge sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays,
 sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
+TC-NlogN
+SC-N
 */
 #include <bits/stdc++.h>
 using namespace std;
